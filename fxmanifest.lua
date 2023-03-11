1475350d4a70506cdcc8e1f1882cf9c693aaa817
RexshackGaming <rexshackuk@gmail.com>
2023-03-11 11:11:25 +0000

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,24 +3,21 @@ rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aw
 game 'rdr3'
 
 client_scripts {
-	'client/main.lua'
+    'client/main.lua'
 }
 
 ui_page {
-	'html/ui.html'
+    'html/ui.html'
 }
 
 files {
-	'html/ui.html',
-	'html/*.png',
-	'html/css/app.css',
-	'html/css/*.png',
-	'html/js/mustache.min.js',
-	'html/js/app.js',
-	'html/js/wrapper.js',
-	'html/fonts/RDRLino-Regular.ttf',
-	'html/fonts/HapnaSlabSerif-Medium.ttf',
-	'html/fonts/bankgothic.ttf'
+    'html/ui.html',
+    'html/*.png',
+    'html/css/app.css',
+    'html/css/*.png',
+    'html/js/mustache.min.js',
+    'html/js/app.js',
+	'html/fonts/*.ttf'
 }
 
 lua54 'yes'
