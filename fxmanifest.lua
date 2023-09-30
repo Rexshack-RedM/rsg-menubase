e7ee6253ee980ef44df2e6abe61078c03a6d3a61
RexshackGaming <rexshackuk@gmail.com>
2023-09-30 14:17:17 +0100

    version checker

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,8 +2,15 @@ fx_version 'cerulean'
 rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
 game 'rdr3'
 
+description 'rsg-menubase'
+version '1.0.0'
+
 client_scripts {
     'client/main.lua'
+}
+
+server_scripts { 
+	'server/server.lua'
 }
 
 ui_page {
